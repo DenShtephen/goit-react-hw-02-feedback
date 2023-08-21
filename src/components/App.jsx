88d064973890e67ca4c1a4faa31bb705d5e294b5
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-08-21 20:28:44 +0300

    do first task and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Statistics } from './Points/Statistics';
+import { FeedbackOptions } from './Points/Feedback';
+import '../index.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leavePoints = point => {
+    this.setState(prevState => ({
+      [point]: prevState[point] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    const { good } = this.state;
+    return totalFeedback > 0 ? ((good / totalFeedback) * 100).toFixed(2) : 0;
+  };
+
+  render() {
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div className="container">
+        <Section title="Please leave feedback" className="section">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.leavePoints}
+          />
+        </Section>
+        <Section title="Statistics" className="section">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
